517b189ead3a23f725d466378747e805acf2e963
Prasant Samal <samal_prasant123@yahoo.co.in>
2018-02-22 22:34:07 -0500

    Method level Role based access - 2

--- src/com/prasant/spring/mvc/service/OfferService.java
+++ src/com/prasant/spring/mvc/service/OfferService.java
@@ -3,6 +3,7 @@ package com.prasant.spring.mvc.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Service;
 
 import com.prasant.spring.mvc.dao.OfferDAO;
@@ -18,6 +19,7 @@ public class OfferService {
 		return offerDao.getOffers();
 	}
 	
+	@Secured({"ROLE_USER", "ROLE_ADMIN"})
 	public boolean createOffer(Offer offer) {
 		return offerDao.create(offer);
 	}
