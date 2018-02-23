eee6c141f01abfe6cb083dcdfddc5866fe1d9fa1
Prasant Samal <samal_prasant123@yahoo.co.in>
2018-02-22 22:20:15 -0500

    Method level Role based access

--- src/com/prasant/spring/mvc/service/UserService.java
+++ src/com/prasant/spring/mvc/service/UserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Service;
 
 import com.prasant.spring.mvc.dao.UserDAO;
@@ -24,6 +25,7 @@ public class UserService {
 		return userDao.getUser(username);
 	}
 
+	@Secured("ROLE_ADMIN")
 	public List<User> getUsers() {
 		return userDao.getUsers();
 	}
